838852b2f28204eb3bcd11de33de0733fa4447be
Mardin <mardinomer001@gmail.com>
2024-02-26 13:38:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    const headquarters = 42;
+    return Math.abs(headquarters - street);
+  }
+  
+  function distanceFromHqInFeet(street) {
+    const block = 264; 
+    return distanceFromHqInBlocks(street) * block;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const block = 264; 
+    return Math.abs(start - destination) * block;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+
+  
